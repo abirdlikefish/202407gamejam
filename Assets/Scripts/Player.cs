9fc386a036b77f5771eadfcc0b13135593f96384
Your Name <email@example.com>
2024-07-06 00:15:41 +0800

    a

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rb;
     private Statemachine _stateMachine;
     private float _speed ;
+    private float _jumpHeight;
 
     private void Awake()
     {
@@ -34,6 +35,12 @@ public class Player : MonoBehaviour
         float mid = InputBuffer.Instance.GetInputDirection();
         //Debug.Log(mid);
         _rb.velocity = new Vector2(mid * _speed , _rb.velocity.y);
+        return _rb.velocity;
+    }
+    public Vector2 Jump()
+    {
+        float jumpSpeed = Mathf.Sqrt(2 * _jumpHeight * Physics2D.gravity.y * -1);
+        _rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed);
         return _rb.velocity;
     }
     public void SetLookDIrection(bool isRight)
